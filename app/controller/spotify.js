d31cb31adef87e820c09ba1d2eb5e800f9f148d9
Gustavo Laguna <gustavo.gelf@gmail.com>
2018-07-23 03:14:24 -0400

    Save albums on mongo on search

--- app/controller/spotify.js
+++ app/controller/spotify.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let Albums = require('../models/album.js');
+let Promise = require('bluebird');
 let spotifyHelper = require('../helpers/spotify');
 
 module.exports = {
@@ -9,6 +11,8 @@ module.exports = {
 async function searchAlbum(req, res, next) {
   let {query} = req;
   let response = await spotifyHelper.search(query.q).catch(next);
-
+  Promise.map(response.albums.items, function storeAlbums(album) {
+  	return Albums.updateOrCreate(album);
+  })
   res.json(response.albums);
 }
